090f3e93900d2e17b2ad6ee80855fdbc4f14e748
Vladislav Kheynov <lcastory86@gmail.com>
2022-08-06 13:05:58 +0700

    initial commit

---
+++ src/main/kotlin/ru/kheynov/Application.kt
@@ -0,0 +1,16 @@
+package ru.kheynov
+
+import io.ktor.server.application.*
+import ru.kheynov.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSecurity()
+    configureHTTP()
+    configureMonitoring()
+    configureSerialization()
+    configureRouting()
+}
