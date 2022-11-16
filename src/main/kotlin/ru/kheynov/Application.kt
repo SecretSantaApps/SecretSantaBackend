11ac73a6b7645721e6d5b92d8cae9685cbe1a207
Vladislav Kheynov <lcastory86@gmail.com>
2022-11-16 21:22:40 +0700

    chore: move plugins to Application.kt

--- src/main/kotlin/ru/kheynov/Application.kt
+++ src/main/kotlin/ru/kheynov/Application.kt
@@ -1,8 +1,22 @@
 package ru.kheynov
 
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import ru.kheynov.plugins.*
+import io.ktor.server.auth.*
+import io.ktor.server.plugins.callloging.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import org.slf4j.event.Level
+import ru.kheynov.api.v1.routing.v1Routes
 import ru.kheynov.security.firebase.auth.FirebaseAdmin
+import ru.kheynov.security.firebase.auth.UserAuth
+import ru.kheynov.security.firebase.auth.firebase
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -15,4 +29,62 @@ fun Application.module() {
     configureMonitoring()
     configureSerialization()
     configureRouting()
+}
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respond("Secret Santa Server")
+        }
+        route("/api") {
+            v1Routes()
+        }
+    }
+}
+
+
+fun Application.configureHTTP() {
+    install(CORS) {
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowMethod(HttpMethod.Patch)
+        allowHeader(HttpHeaders.Authorization)
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+}
+
+fun Application.configureMonitoring() {
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+}
+
+fun Application.configureFirebaseAuth() {
+    install(Authentication) {
+        firebase {
+            validate {
+                UserAuth(it.uid, it.name)
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalSerializationApi::class)
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json(
+            Json {
+                encodeDefaults = false
+                explicitNulls = false
+            }
+        )
+    }
+
+    routing {
+        get("/json/kotlinx-serialization") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
 }
