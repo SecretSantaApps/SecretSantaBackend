cbfb6ed59b60bf567c89db70d1521fb08c6dbb3b
Vladislav Kheynov <lcastory86@gmail.com>
2022-11-16 21:23:00 +0700

    feat: add new routes, full implement backend

--- src/main/kotlin/ru/kheynov/api/v1/routing/GameRoutes.kt
+++ src/main/kotlin/ru/kheynov/api/v1/routing/GameRoutes.kt
@@ -6,13 +6,9 @@ import io.ktor.server.auth.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import ru.kheynov.api.v1.requests.game.JoinRoomRequest
-import ru.kheynov.api.v1.requests.game.KickUserRequest
-import ru.kheynov.api.v1.requests.game.LeaveRoomRequest
+import ru.kheynov.api.v1.requests.game.*
 import ru.kheynov.domain.use_cases.UseCases
-import ru.kheynov.domain.use_cases.game.JoinRoomUseCase
-import ru.kheynov.domain.use_cases.game.KickUserUseCase
-import ru.kheynov.domain.use_cases.game.LeaveRoomUseCase
+import ru.kheynov.domain.use_cases.game.*
 import ru.kheynov.security.firebase.auth.FIREBASE_AUTH
 import ru.kheynov.security.firebase.auth.UserAuth
 
@@ -32,9 +28,7 @@ fun Route.configureGameRoutes(
                         return@post
                     }
                     val res = useCases.joinRoomUseCase(
-                        userId = user.userId,
-                        roomName = request.roomName,
-                        password = request.password
+                        userId = user.userId, roomName = request.roomName, password = request.password
                     )
                     when (res) {
                         JoinRoomUseCase.Result.Failed -> TODO()
@@ -57,8 +51,7 @@ fun Route.configureGameRoutes(
                         return@post
                     }
                     val res = useCases.leaveRoomUseCase(
-                        userId = user.userId,
-                        roomName = request.roomName
+                        userId = user.userId, roomName = request.roomName
                     )
                     when (res) {
                         LeaveRoomUseCase.Result.Failed -> TODO()
@@ -85,7 +78,7 @@ fun Route.configureGameRoutes(
                         userId = request.userId,
                         roomName = request.roomName,
                     )
-                    when(res){
+                    when (res) {
                         KickUserUseCase.Result.Failed -> TODO()
                         KickUserUseCase.Result.Forbidden -> TODO()
                         KickUserUseCase.Result.GameAlreadyStarted -> TODO()
@@ -98,12 +91,96 @@ fun Route.configureGameRoutes(
             }
             authenticate(FIREBASE_AUTH) {
                 post("/start") {
-                    TODO()
+                    val user = call.principal<UserAuth>() ?: run {
+                        call.respond(HttpStatusCode.Unauthorized)
+                        return@post
+                    }
+                    val request = call.receiveNullable<StartGameRequest>() ?: run {
+                        call.respond(HttpStatusCode.BadRequest)
+                        return@post
+                    }
+                    val res = useCases.startGameUseCase(
+                        userId = user.userId, roomName = request.roomName
+                    )
+
+                    when (res) {
+                        StartGameUseCase.Result.Failed -> {
+                            call.respond(HttpStatusCode.BadRequest, "Something went wrong")
+                            return@post
+                        }
+
+                        StartGameUseCase.Result.Forbidden -> {
+                            call.respond(HttpStatusCode.Forbidden)
+                            return@post
+                        }
+
+                        StartGameUseCase.Result.GameAlreadyStarted -> {
+                            call.respond(HttpStatusCode.Conflict, "Game already started")
+                            return@post
+                        }
+
+                        StartGameUseCase.Result.RoomNotFound -> {
+                            call.respond(HttpStatusCode.BadRequest, "Room not found")
+                            return@post
+                        }
+
+                        StartGameUseCase.Result.Successful -> {
+                            call.respond(HttpStatusCode.OK)
+                            return@post
+                        }
+
+                        StartGameUseCase.Result.UserNotFound -> {
+                            call.respond(HttpStatusCode.BadRequest, "User not found")
+                            return@post
+                        }
+                    }
                 }
             }
             authenticate(FIREBASE_AUTH) {
                 post("/stop") {
-                    TODO()
+                    val user = call.principal<UserAuth>() ?: run {
+                        call.respond(HttpStatusCode.Unauthorized)
+                        return@post
+                    }
+                    val request = call.receiveNullable<StopGameRequest>() ?: run {
+                        call.respond(HttpStatusCode.BadRequest)
+                        return@post
+                    }
+                    val res = useCases.stopGameUseCase(
+                        userId = user.userId,
+                        roomName = request.roomName
+                    )
+                    when (res) {
+                        StopGameUseCase.Result.Failed -> {
+                            call.respond(HttpStatusCode.BadRequest, "Something went wrong")
+                            return@post
+                        }
+
+                        StopGameUseCase.Result.Forbidden -> {
+                            call.respond(HttpStatusCode.Forbidden)
+                            return@post
+                        }
+
+                        StopGameUseCase.Result.GameAlreadyStopped -> {
+                            call.respond(HttpStatusCode.Conflict, "Game already stopped")
+                            return@post
+                        }
+
+                        StopGameUseCase.Result.RoomNotFound -> {
+                            call.respond(HttpStatusCode.BadRequest, "Room not found")
+                            return@post
+                        }
+
+                        StopGameUseCase.Result.Successful -> {
+                            call.respond(HttpStatusCode.OK)
+                            return@post
+                        }
+
+                        StopGameUseCase.Result.UserNotFound -> {
+                            call.respond(HttpStatusCode.BadRequest, "User not found")
+                            return@post
+                        }
+                    }
                 }
             }
         }
