369b7364bb417c1b92bd4736e1dc66598bcee976
Vladislav Kheynov <lcastory86@gmail.com>
2022-11-21 12:09:55 +0700

    feat: add sql query

---
+++ sql/get_users.sql
@@ -0,0 +1,20 @@
+create or replace function get_relations(roomName text)
+    returns table
+            (
+                giver     text,
+                recipient text
+            )
+as
+$$
+begin
+    return query select giver.name     as giver,
+                        recipient.name as recipient
+                 from room_members
+                          inner join users giver on giver.user_id = room_members.user_id
+                          inner join users recipient on recipient.user_id = room_members.recipient
+                 where room_members.room_name = roomName;
+end;
+$$ language plpgsql;
+
+select *
+from get_relations('room_for_my_homies');
