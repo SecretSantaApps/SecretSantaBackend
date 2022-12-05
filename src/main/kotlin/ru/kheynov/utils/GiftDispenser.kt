0ae3f91f55456ef3032357c521504cf3b50ba879
Vladislav Kheynov <lcastory86@gmail.com>
2022-12-05 23:11:31 +0700

    fix: change dispenser algorithm

--- src/main/kotlin/ru/kheynov/utils/GiftDispenser.kt
+++ src/main/kotlin/ru/kheynov/utils/GiftDispenser.kt
@@ -7,8 +7,9 @@ interface GiftDispenser {
 class SimpleCycleGiftDispenser : GiftDispenser {
     override fun getRandomDistribution(users: List<String>): List<Pair<String, String>> {
         val result: MutableList<Pair<String, String>> = mutableListOf()
-
-        users.forEachIndexed { index, userId ->
+        val tmp = users.toMutableList()
+        tmp.shuffle()
+        tmp.forEachIndexed { index, userId ->
             if (index == users.size - 1) {
                 result.add(userId to users[0])
             } else {
